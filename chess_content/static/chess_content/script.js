d087a99383a0530412931423d61cff9c3e2d716a
Atakan <atakansolak28@hotmail.com>
2023-08-17 17:48:41 +0300

    scrolling bug fixed

--- chess_content/static/chess_content/script.js
+++ chess_content/static/chess_content/script.js
@@ -1,6 +1,10 @@
-document.addEventListener('DOMContentLoaded', () => { 
-    const pieces = document.getElementsByClassName("chess_pieces");
-    for (let piece of pieces) {
+document.addEventListener('DOMContentLoaded', () => {
+    const chessPieces = document.getElementsByClassName("chess_pieces");
+    const trashPieces = document.getElementsByClassName("trash");
+    
+    const allPieces = [...chessPieces, ...trashPieces];
+
+    for (let piece of allPieces) {
         piece.addEventListener("click", choosePiece);
     }
 });
@@ -8,19 +12,19 @@ document.addEventListener('DOMContentLoaded', () => {
 
 let activePiece = null;
 
-const boardRect = document.querySelector(".memory_board").getBoundingClientRect(); 
 
 let previouslyClickedPiece = null;
 
 
 function choosePiece(event) {
+    const clickedPiece = event.target;
+    
     if (activePiece) {
         document.removeEventListener("mousemove", movePiece);
         activePiece.remove();
         activePiece = null;  // Make sure to nullify this to prevent lingering references
     }
 
-    const clickedPiece = event.target;
 
     // Reset the background color of the previously clicked piece
     if (previouslyClickedPiece) {
@@ -28,10 +32,16 @@ function choosePiece(event) {
         previouslyClickedPiece.classList.remove("animate-background");
     }
 
-    clickedPiece.classList.add("animate-background");
-    clickedPiece.style.backgroundColor = "#139feb93";
+    if (event.target.className != "trash"){
+        clickedPiece.classList.add("animate-background");
+        clickedPiece.style.backgroundColor = "#16b0df";
+    } else if (event.target.className == "trash") {
+        clickedPiece.classList.add("animate-background");
+        clickedPiece.style.backgroundColor = "#f57878f6";
+    }
     
     previouslyClickedPiece = clickedPiece;  // Store the reference to the current clicked piece for future use
+
 
     activePiece = clickedPiece.cloneNode(true);
     Object.assign(activePiece.style, {
@@ -50,37 +60,56 @@ function choosePiece(event) {
 }
 
 
+
 function movePiece(event) {
     if (!activePiece) return;
 
-    activePiece.style.left = `${event.clientX - activePiece.width / 2}px`;
-    activePiece.style.top = `${event.clientY - activePiece.height / 2}px`;
+    activePiece.style.left = `${event.clientX + window.scrollX - activePiece.width / 2}px`;
+    activePiece.style.top = `${event.clientY + window.scrollY - activePiece.height / 2}px`;
 }
+
+let lastMouseEvent = null;
+
+document.addEventListener('mousemove', (event) => {
+    lastMouseEvent = event;
+});
+
+document.addEventListener('scroll', function() {
+    if (activePiece && lastMouseEvent) {
+        activePiece.style.left = `${lastMouseEvent.clientX + window.scrollX - activePiece.width / 2}px`;
+        activePiece.style.top = `${lastMouseEvent.clientY + window.scrollY - activePiece.height / 2}px`;
+    }
+});
 
 
 document.querySelector(".memory_board").addEventListener("click", placePiece);
 
 function placePiece(event) {
-    if (!activePiece) return;
+    if (!activePiece || activePiece.className == "trash animate-background") return;
+
+    // Calculate the boardRect here, inside the placePiece function
+    const boardRect = document.querySelector(".memory_board").getBoundingClientRect();
 
     const { left: boardX, top: boardY } = boardRect;
-    const x = event.clientX;
-    const y = event.clientY;
+
+    // Use pageX and pageY for coordinates relative to the whole document
+    const x = event.pageX; 
+    const y = event.pageY; 
 
     const squareWidth = activePiece.width;  
     const squareHeight = activePiece.height;
 
-    if (x >= boardX && x <= boardX + 8 * squareWidth && y <= boardY + 8 * squareHeight && y >= boardY) {
-        const [Xcord, Ycord] = [Math.floor((x - boardX) / squareWidth), Math.floor((y - boardY) / squareHeight)];
+    if (x >= boardX + window.scrollX && x <= boardX + window.scrollX + 8 * squareWidth && y <= boardY + window.scrollY + 8 * squareHeight && y >= boardY + window.scrollY) {
+        const [Xcord, Ycord] = [Math.floor((x - boardX - window.scrollX) / squareWidth), Math.floor((y - boardY - window.scrollY) / squareHeight)];
         const squareCenter = [Xcord * squareWidth + squareWidth / 2, Ycord * squareHeight + squareHeight / 2];
         
         const duplicate = activePiece.cloneNode(true);
         Object.assign(duplicate.style, {
             position: "absolute",
-            left: `${squareCenter[0] - squareWidth / 2 + boardX}px`,
-            top: `${squareCenter[1] - squareHeight / 2 + boardY}px`,
+            left: `${squareCenter[0] - squareWidth / 2 + boardX + window.scrollX}px`,
+            top: `${squareCenter[1] - squareHeight / 2 + boardY + window.scrollY}px`,
             zIndex: "9997",
-            pointerEvents: "auto"  // <-- Adjusted this to allow events on the duplicate
+            pointerEvents: "auto"  // Allow events on the duplicate
         });
         
         duplicate.classList.add('duplicate-piece');  // Assign a specific class to each duplicated piece
@@ -88,6 +117,9 @@ function placePiece(event) {
         document.body.appendChild(duplicate);
     }
 }
+
+
+
 
 
 document.addEventListener('click', function(event) {
